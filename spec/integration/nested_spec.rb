af6408c71a0b0951021d64e6e02b2c48dc1ac4cf
Saverio Trioni <saverio.trioni@kantox.com>
2017-03-20 10:15:30 +0100

    Simpler failing spec

--- spec/integration/nested_spec.rb
+++ spec/integration/nested_spec.rb
@@ -1,68 +1,48 @@
 RSpec.describe Dry::Configurable do
-  context 'with nested configuration and a subclass' do
-    let(:base_base_klass) do
+  context 'with nested configuration' do
+    let(:base_klass) do
       Class.new do
         extend Dry::Configurable
 
         setting :nested do
-          setting :thing, 42
+          setting :thing, 'from base klass'
         end
       end
     end
 
-    let(:base_klass) do
-      Class.new(base_base_klass) do
-        setting :unrelated
-      end
-    end
-
-    shared_examples 'superclass config is not clobbered' do
-      it 'reading base class config before' do
-        expect(base_klass.config.nested.thing).to eq 42
-        expect(klass.config.nested.thing).to eq 3.14
-      end
-
-      it 'reading subclass config before' do
-        expect(klass.config.nested.thing).to eq 3.14
-        expect(base_klass.config.nested.thing).to eq 42
-      end
-
-      context 'independent configuration of superclasses' do
-        before do
-          base_base_klass.config.nested.thing = 84
-          base_klass.config.nested.thing = 168
-        end
-
-        it 'has all distinct values' do
-          expect(base_base_klass.config.nested.thing).to eq 84
-          expect(base_klass.config.nested.thing).to eq 168
-          expect(klass.config.nested.thing).to eq 3.14
+    let(:klass) do
+      Class.new(base_klass) do
+        configure do |k|
+          k.nested.thing = 'from klass'
         end
       end
     end
 
-    context 'when setting is configured inside' do
-      let(:klass) do
-        Class.new(base_klass) do
-          configure do |k|
-            k.nested.thing = 3.14
-          end
+    let(:other_klass) do
+      Class.new(base_klass) do
+        configure do |k|
+          k.nested.thing = 'from other klass'
         end
       end
-
-      include_examples 'superclass config is not clobbered'
     end
 
-    context 'when setting is configured from outside' do
-      let(:klass) do
-        Class.new(base_klass)
-      end
+    it 'subclasses do not clobber each other' do
+      expect(base_klass.config.nested.thing).to eq 'from base klass'
+      expect(klass.config.nested.thing).to eq 'from klass'
+      expect(other_klass.config.nested.thing).to eq 'from other klass'
 
-      before do
-        klass.config.nested.thing = 3.14
-      end
+      # the following fails, as it has been then changed by the configure block
+      # of other_klass.
+      expect(klass.config.nested.thing).to eq 'from klass'
+    end
 
-      include_examples 'superclass config is not clobbered'
+    it 'subclasses respect superclass own config' do
+      expect(base_klass.config.nested.thing).to eq 'from base klass'
+      expect(klass.config.nested.thing).to eq 'from klass'
+
+      # the following fails, as it has been then changed by the configure block
+      # of klass.
+      expect(base_klass.config.nested.thing).to eq 'from base klass'
     end
   end
 end
